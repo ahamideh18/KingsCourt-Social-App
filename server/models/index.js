9d791f633e04d6c2e8e4b29505cae324fd0451f2
ahamideh <ahamideh18@hotmail.com>
2020-08-29 13:58:55 -0400

    Migrated to MONGO Atlas

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/kc-app', {
+mongoose.connect(`mongodb+srv://kc-admin:${process.env.MDB_PASSWORD}@cluster0.me1l8.mongodb.net/kc-app?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
